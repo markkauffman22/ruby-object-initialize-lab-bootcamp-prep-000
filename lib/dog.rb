16ab90c65aa0983653dad6ea8c791a04f718f33d
Mark Kauffman <markkauffman22@gmail.com>
2019-10-17 17:22:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-  def initialize(name, breed = "Mutt") # Note - no need to pass mandatory args.
+  def initialize(name, breed = "Mutt") # Note - name is mandatory args, breed is optional w/default "Mutt"
     @name = name
     @breed = breed
   end  
